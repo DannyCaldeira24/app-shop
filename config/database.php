d7c5528dcd1711997f43c5e81327f2ae79eed044
Danny Caldeira <dannyelportu2013@gmail.com>
2018-09-18 19:24:44 -0400

    Config DB

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_109531c5406690d'),
+            'username' => env('DB_USERNAME', 'b6e40d1519aef0'),
+            'password' => env('DB_PASSWORD', 'bcb966fa'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
