9e1f77d0fc474849bf7ae0402d2998c42ec9bcaf
Danny Caldeira <dannyelportu2013@gmail.com>
2018-09-19 04:15:28 -0400

    Añadiendo admin para testing

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,6 +22,15 @@ class UsersTableSeeder extends Seeder
             'admin'=>true
         ]);
         User::create([
+            'name'=>'Admin',
+            'surname'=>'Test',
+            'direction'=>'Hello word',
+            'postal_code'=>'1111',
+            'email'=>'admin@example.com',
+            'password'=>bcrypt('123123'),
+            'admin'=>false
+        ]);
+        User::create([
             'name'=>'Clara Josefina',
             'surname'=>'Lozano Lipez',
             'direction'=>'Madrid-España',
