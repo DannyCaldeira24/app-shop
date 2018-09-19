84bea162ccd75da5294c64dcc2d77e55d21f8243
Danny Caldeira <dannyelportu2013@gmail.com>
2018-09-19 04:28:58 -0400

    Modificando rol de admin a true al usuario Admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -37,7 +37,7 @@ class UsersTableSeeder extends Seeder
             'postal_code'=>'1095',
             'email'=>'danny_elportu@hotmail.com',
             'password'=>bcrypt('123123'),
-            'admin'=>false
+            'admin'=>true
         ]);
     }
 }
